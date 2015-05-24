6c1bb15c6a26e9baa5a83dd233c13f078f552c60
Dmitry Sedov <sedov.dmitry1@gmail.com>
2015-05-25 01:59:35 +0300

    finished unit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,16 @@ class PostsController < ApplicationController
     @new_post = Post.new
     @all_posts = Post.order(created_at: :desc).all
   end
+
+  def create 
+    @new_post = Post.new(post_params)
+    if @new_post.save
+      redirect_to '/'
+    end
+  end
+
+    private
+    def post_params
+      params.require(:post).permit(:comment)
+    end
 end
